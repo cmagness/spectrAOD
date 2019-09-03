ea5b5ea6dabb617f731e0a4fbfed3e4d2ebfbbc2
Camellia Magness <cmagness@stsci.edu>
2019-09-03 14:58:09 -0400

    configured .yaml settings options. should close #17

--- spectrAOD/measure_aod.py
+++ spectrAOD/measure_aod.py
@@ -11,19 +11,36 @@ __email__ = "cmagness@stsci.edu"
 import argparse
 import sys
 
-from format_data import *
+from . import SETTINGS
+from .format_data import build_spectrum
+from .spectrum_classes import Helper
 
-# from astropy import constants
-
-# C = constants.c.to('km/s')  # km/s
-
-DATADIR = "/user/cmagness/fermi/data/15339/UVQSJ191928-295808/x1d/"
-OUTDIR = "/user/cmagness/fermi/data/out/"
-
-TARGETS = "/user/cmagness/fermi/code/agn_target_list.csv"
+INPUTS = SETTINGS["inputs"]
+DATADIR = INPUTS["datadir"]
+TARGETS = INPUTS["targets"]
+PARAMETERS = SETTINGS["parameters"]
+DEFAULTS = SETTINGS["defaults"]
 
 
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------- #
+
+
+def main():
+    # argparse
+    args, spectrum = parse()
+    # LSR correction
+    spectrum = lsr_correct(args, spectrum)
+    # continuum fit
+    spectrum = continuum_fit(spectrum)
+    # measure aod/acd/ew
+    # set measurements back in spectrum object from helper object
+    spectrum = measure(args, spectrum)
+    # generate table
+    spectrum.generate_table(args.vel_min, args.vel_max)
+    return 0
+
+
+# --------------------------------------------------------------------------- #
 
 
 def parse():
@@ -31,20 +48,28 @@ def parse():
 
     parser = argparse.ArgumentParser(description="spectrAOD")
 
-    parser.add_argument("--instrument", default="COS", type=str,
-                        help="observational instrument data is taken on")
-    parser.add_argument("--filetype", default="X1DSUM", type=str,
-                        help="file type of data")
-    parser.add_argument("--ion", default="SiIV", type=str,
+    # for debug only
+    parser.add_argument("--ion", default="SiIII", type=str,
                         help="absorption line feature of interest")
-    parser.add_argument("--vel_min", default="-100", type=int,
+
+    # parser.add_argument("ion", type=str,
+    #                     help="absorption line feature of interest")
+    parser.add_argument("--instrument", default=PARAMETERS["instrument"],
+                        type=str, help="observational instrument data is taken"
+                                       "on")
+    parser.add_argument("--filetype", default=PARAMETERS["filetype"], type=str,
+                        help="filetype of data")
+    parser.add_argument("--vel_min", default=PARAMETERS["vel_min"], type=int,
                         help="velocity minimum in window of interest around "
                              "ion, in km/s")
-    parser.add_argument("--vel_max", default="100", type=int,
+    parser.add_argument("--vel_max", default=PARAMETERS["vel_max"], type=int,
                         help="velocity maximum in window of interest around "
                              "ion, in km/s")
-    parser.add_argument("--grating", default="G130M", type=str,
-                        help="optional grating to choose from")
+    parser.add_argument("--grating", default=PARAMETERS["grating"], type=str,
+                        help="grating of observation mode")
+    # need to do something about the grating bc not every dataset will have
+    # a grating in the future
+
     # need to set up better debugging system than defaults. debugging flag?
     # need to set default grating to something for creating Spectrum object
     # NEED TO DO SOMETHING ABOUT THE FACT THAT COULD HAVE ION LIST IN THE
@@ -55,20 +80,13 @@ def parse():
     # range of the grating
 
     args = parser.parse_args()
-    # a = vars(args)
-    if args.grating:
-        spectrum = collect(DATADIR, args.instrument, args.filetype,
-                           args.grating)
-    else:
-        spectrum = collect(DATADIR, args.instrument, args.filetype)
-
-    # switch the arguments to unpacking with vars() and switch collect to
-    # accepting **kw args
+    spectrum = build_spectrum(DATADIR, args.instrument, args.filetype,
+                              args.grating)
 
     return args, spectrum
 
 
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------- #
 
 
 def lsr_correct(args, spectrum):
@@ -89,10 +107,11 @@ def lsr_correct(args, spectrum):
     return spectrum
 
 
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------- #
 
 
-def continuum_fit(spectrum, left=[-450, -300], right=[300, 450]):
+def continuum_fit(spectrum, left=DEFAULTS["continuum_left"],
+                  right=DEFAULTS["continuum_right"]):
     """This function performs all the calculations relating to the
     continuum, as well as calculating the normalized flux
     and error arrays"""
@@ -105,19 +124,15 @@ def continuum_fit(spectrum, left=[-450, -300], right=[300, 450]):
                                                    right_indices)]
     # calculating means, signal to noise, pixel size, and S/N per resel
     continuum, signalnoise, pixels = spectrum.calculate_continuum(indices)
-    # WE WANT TO ADD THE SN_AVG AS AN ATTRIBUTE FOR THE TABLE AT SOME POINT
-    # PROBABLY
+
     # calculating fits to continuum windows and updating normalized flux and
     # error attributes
-
-    # note: pixels is empty at this point after the test run
-    # this needs investigating
     spectrum, linear_fits = spectrum.calculate_fits(continuum)
 
     return spectrum
 
 
-# -----------------------------------------------------------------------------
+# --------------------------------------------------------------------------- #
 
 
 def measure(args, spectrum):
@@ -144,24 +159,6 @@ def measure(args, spectrum):
     spectrum.set_measurements(helper)
 
     return spectrum
-
-
-# -----------------------------------------------------------------------------
-
-
-def main():
-    # argparse
-    args, spectrum = parse()
-    # LSR correction
-    spectrum = lsr_correct(args, spectrum)
-    # continuum fit
-    spectrum = continuum_fit(spectrum)
-    # measure aod/acd/ew
-    # set measurements back in spectrum object from helper object
-    spectrum = measure(args, spectrum)
-    # generate table
-    spectrum.generate_table(args.vel_min, args.vel_max)
-    return 0
 
 
 # -----------------------------------------------------------------------------
